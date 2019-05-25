8cbc986a144903ca2d007c19ffec08876561f7a1
Shawn Wheeler <shawnrwheeler@gmail.com>
2019-05-25 20:15:35 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,8 +1,7 @@
 def reformat_languages(languages)
   new_languages = {}
   languages.each do |style, language|
-    language.each do |key, value|
-      new_languages.push(key => value)
+    new_languages = language.map.flatten do |key, value|
     end
   end
   new_languages
